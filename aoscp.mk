89aeff0270705219389f4d35f57b34f339d8ab58
Sahul Krishan <yt.sahulkrishan@gmail.com>
2018-06-01 08:40:53 -0700

    sirius: AOSCP-ify

--- aoscp.mk
+++ aoscp.mk
@@ -1,8 +1,8 @@
-## Inherit Carbon GSM telephony parts
-$(call inherit-product, vendor/carbon/config/gsm.mk)
+# Inherit CypherOS GSM telephony parts
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
 
-# Inherit Carbon product configuration
-$(call inherit-product, vendor/carbon/config/common.mk)
+# Inherit CypherOS product configuration
+$(call inherit-product, vendor/aoscp/configs/common.mk)
 
 $(call inherit-product, device/sony/sirius/device.mk)
 
@@ -11,8 +11,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6503-user 6.0.1 23.5.A.1.29
 
 BUILD_FINGERPRINT := Sony/D6503/D6503:6.0.1/23.5.A.1.291/2769308465:user/release-keys
 
-PRODUCT_NAME := carbon_sirius
+PRODUCT_NAME := aoscp_sirius
 PRODUCT_DEVICE := sirius
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.carbon.maintainer="Rcstar6696"
